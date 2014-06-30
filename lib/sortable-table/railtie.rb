948daade7ed4af5861a2712d1c2570f8fb1a984d
Jay Mitchell <jaybmitchell@gmail.com>
2014-06-30 16:07:36 -0600

    Added requires to railtie.

--- lib/sortable-table/railtie.rb
+++ lib/sortable-table/railtie.rb
@@ -1,6 +1,11 @@
 module SortableTable
   class Railtie < Rails::Railtie
     initializer 'sortable-table' do |app|
+      require 'sortable-table/models/sort_column'
+      require 'sortable-table/models/sort_column_custom_definition'
+      require 'sortable-table/models/sort_column_definition'
+      require 'sortable-table/models/sort_table'
+      
       ActiveSupport::on_load(:action_view) do
         ActionView::Base.send :include, SortableTable::ActionViewExtension
       end
