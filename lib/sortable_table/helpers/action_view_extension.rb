a6a08a29dc87a65408d42d890e7d083b3039087e
Andrew Porter <andrew.porter@intercity-uk.com>
2015-05-02 15:05:36 +0100

    Enable sort_by helper options to be passed through to link_to
    This enables the ability to add remote: true etc.. to links

--- lib/sortable_table/helpers/action_view_extension.rb
+++ lib/sortable_table/helpers/action_view_extension.rb
@@ -1,16 +1,16 @@
 module SortableTable
   module ActionViewExtension
     def sort_by(column, options = {})
-      current_column = options[:current_column] || SortableTable::SortColumn.new(nil, nil)
-      title = options[:title] || column.titleize
-      prefix = options[:prefix]
+      current_column = options.delete(:current_column) || SortableTable::SortColumn.new(nil, nil)
+      title = options.delete(:title) || column.titleize
+      prefix = options.delete(:prefix)
       is_current_column = column == current_column.column
       css_class = is_current_column ? "current #{current_column.direction}" : nil
       direction = is_current_column && current_column.direction == 'asc' ? 'desc' : 'asc'
       link_to title, params.merge("#{prefix}sort" => column,
                                   "#{prefix}direction" => direction,
                                   "#{prefix}page" => nil),
-              class: css_class
+              options.merge( {class: css_class } )
     end
   end
 end
